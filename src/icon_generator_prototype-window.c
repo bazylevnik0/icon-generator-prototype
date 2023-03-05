9516a9ea2fef8c2e40fc6da887939a99193aef01
bazylevnik0 <bazylevnik0@gmail.com>
2023-03-05 12:26:56 +0300

    clear builder project

---
+++ src/icon_generator_prototype-window.c
@@ -0,0 +1,47 @@
+/* icon_generator_prototype-window.c
+ *
+ * Copyright 2023 nik0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "icon_generator_prototype-config.h"
+#include "icon_generator_prototype-window.h"
+
+struct _IconGeneratorPrototypeWindow
+{
+  GtkApplicationWindow  parent_instance;
+
+  /* Template widgets */
+  GtkHeaderBar        *header_bar;
+  GtkLabel            *label;
+};
+
+G_DEFINE_TYPE (IconGeneratorPrototypeWindow, icon_generator_prototype_window, GTK_TYPE_APPLICATION_WINDOW)
+
+static void
+icon_generator_prototype_window_class_init (IconGeneratorPrototypeWindowClass *klass)
+{
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/icon/generator/prototype/icon_generator_prototype-window.ui");
+  gtk_widget_class_bind_template_child (widget_class, IconGeneratorPrototypeWindow, header_bar);
+  gtk_widget_class_bind_template_child (widget_class, IconGeneratorPrototypeWindow, label);
+}
+
+static void
+icon_generator_prototype_window_init (IconGeneratorPrototypeWindow *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
