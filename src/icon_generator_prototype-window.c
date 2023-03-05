530d59fd30d645d484edcf042499e2baa54ea803
bazylevnik0 <bazylevnik0@gmail.com>
2023-03-05 15:13:48 +0300

    add some basic ui

--- src/icon_generator_prototype-window.c
+++ src/icon_generator_prototype-window.c
@@ -24,8 +24,7 @@ struct _IconGeneratorPrototypeWindow
   GtkApplicationWindow  parent_instance;
 
   /* Template widgets */
-  GtkHeaderBar        *header_bar;
-  GtkLabel            *label;
+  //GtkGrid               *grid_global;
 };
 
 G_DEFINE_TYPE (IconGeneratorPrototypeWindow, icon_generator_prototype_window, GTK_TYPE_APPLICATION_WINDOW)
@@ -36,12 +35,12 @@ icon_generator_prototype_window_class_init (IconGeneratorPrototypeWindowClass *k
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/icon/generator/prototype/icon_generator_prototype-window.ui");
-  gtk_widget_class_bind_template_child (widget_class, IconGeneratorPrototypeWindow, header_bar);
-  gtk_widget_class_bind_template_child (widget_class, IconGeneratorPrototypeWindow, label);
+  //gtk_widget_class_bind_template_child (widget_class, IconGeneratorPrototypeWindow, grid_global);
 }
 
 static void
 icon_generator_prototype_window_init (IconGeneratorPrototypeWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
+
 }
